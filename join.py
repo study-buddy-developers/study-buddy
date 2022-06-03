c0148171ba0d10b4f3f6820bf088ce027a8ebfbb
joshuan98 <dzhei98@gmail.com>
2022-06-03 15:05:45 +0800

    Refactor code into distinct .py files

---
+++ join.py
@@ -0,0 +1,10 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackContext
+
+
+def join_date(update, context):
+    return
+
+
+def join_time(update, context):
+    return
